a23748190db47bafc4fd8fbb8793f7c0935f4455
pratik nikale <prnikale@gmail.com>
2023-06-20 19:41:23 +0530

    added production db api link

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo?retryWrites=true&w=majority", //production
-  url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo-development?retryWrites=true&w=majority", //development
+  url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo?retryWrites=true&w=majority", //production
+  // url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo-development?retryWrites=true&w=majority", //development
 };
 
 // module.exports = {
