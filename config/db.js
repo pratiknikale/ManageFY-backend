59cba0f01f5fbe444db2c45818c605997a0baad8
pratik nikale <prnikale@gmail.com>
2023-06-20 19:10:10 +0530

    added config and updated gitIgnore

---
+++ config/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo?retryWrites=true&w=majority", //production
+  url: "mongodb+srv://pratikN:ROLLno256@cluster0.1a8wu.mongodb.net/todo-development?retryWrites=true&w=majority", //development
+};
+
+// module.exports = {
+//   localUrl: "http://localhost:3000",
+//   liveUrl: "https://managefy.netlify.app/",
+// };
